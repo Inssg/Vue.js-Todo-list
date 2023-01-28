d51c6f54158e7fabb5297555eab9b7d7154caa2b
Inssg <nightwalker0131@naver.com>
2023-01-28 15:08:39 +0900

    뷰 애니메이션, 모달 추가

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <TodoHeader></TodoHeader>
     <TodoInput v-on:addTodo="addTodo"></TodoInput>
-    <TodoList v-bind:propsdata="todoItems"></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -15,17 +15,32 @@ import TodoFooter from './components/TodoFooter.vue'
 
 export default {
   data() {
-    return{
+    return {
       todoItems: []
     }
   },
   methods: {
-    addTodo(todoItem){
-      localStorage.setItem(todoItem,todoItem);
-      this.todoItems.push(todoItem);
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+		addTodo(todoItem) {
+			localStorage.setItem(todoItem, todoItem);
+			this.todoItems.push(todoItem);
+		},
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
     }
   },
-  components:{
+  created() {
+		if (localStorage.length > 0) {
+			for (var i = 0; i < localStorage.length; i++) {
+				this.todoItems.push(localStorage.key(i));
+			}
+		}
+  },
+  components: {
     'TodoHeader': TodoHeader,
     'TodoInput': TodoInput,
     'TodoList': TodoList,
@@ -35,18 +50,18 @@ export default {
 </script>
 
 <style>
-  body{
+  body {
     text-align: center;
-    background-color: #f6f6f6;
+    background-color: #F6F6F8;
   }
-  input{
+  input {
     border-style: groove;
     width: 200px;
   }
-  button{
+  button {
     border-style: groove;
   }
-  .shadow{
-    box-shadow: 5px 10px rgba(0, 0, 0, 0, 0.03)
+  .shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
   }
 </style>
